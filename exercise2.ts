adcfff4004ad7d12340826ef6a53a656a29afaa5
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-07-20 23:08:31 +0200

    upd

--- exercise2.ts
+++ exercise2.ts
@@ -1,31 +1,37 @@
 // Exercise 1
-var double = (value) => {
-    return value * 2;
-};
-console.log(double(10));
-
-/*// Solution
 var double = function(value) {
     return value * 2;
 };
 console.log(double(10));
 
+// Solution 1
+const double1 = (value) => value * 2;
+console.log(double1(10));
+
 // Exercise 2
-var greet = function (name) {
-    if (name === undefined) { name = "Max"; }
+const greet2 = function (name) {
+    if (name === undefined) { name = "Zhenja"; }
     console.log("Hello, " + name);
 };
-greet();
-greet("Anna");
+// greet2();
+greet2("Anna");
+
+// Solution 2
+const greet1 = (name = "Zhenjha") => {
+    if (name === undefined)
+    console.log("Hello, " + name);
+};
+greet1();
+greet1("Anna");
 
 // Exercise 3
-var numbers = [-3, 33, 38, 5];
-console.log(Math.min.apply(Math, numbers));
+const numbers2 = [-3, 33, 38, 5];
+console.log(Math.min.apply(Math, numbers2));
 
 // Exercise 4
-var newArray = [55, 20];
-Array.prototype.push.apply(newArray, numbers);
-console.log(newArray);
+// const newArray = [55, 20];
+// Array.prototype.push.apply(newArray, numbers);
+// console.log(newArray);
 
 // Exercise 5
 var testResults = [3.89, 2.99, 1.38];
@@ -38,4 +44,4 @@ console.log(result1, result2, result3);
 var scientist = {firstName: "Will", experience: 12};
 var firstName = scientist.firstName;
 var experience = scientist.experience;
-console.log(firstName, experience);*/
+console.log(firstName, experience);
