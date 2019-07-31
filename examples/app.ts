6c0128474588e806ec07aacf21c1ba7b9f2d5d87
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-07-31 07:21:10 +0200

    upd

--- examples/app.ts
+++ examples/app.ts
@@ -1,6 +1,6 @@
 class Person {
     name: string | undefined;
-    private type: string | undefined;
+    protected type: string | undefined;
     protected age: number | undefined;
 
     constructor(name: string, public username: string) {
@@ -22,3 +22,27 @@ const person = new Person("Evgeny", "Zhenja");
 console.log(person.name, person.username);
 person.printAge();
 // person.setType("Cool guy");
+
+// 59. Inheritance
+/*
+class EvgenyClass extends Person {
+    name = "Natasha";
+}
+const myPrint = new EvgenyClass("Anna", "Super");
+console.log(myPrint);
+*/
+
+// 60. Inheritance and Constructor
+class EvgenyClass extends Person {
+    name = "Natasha";
+
+    constructor(props, super1: string) {
+        super(name, "Kolja");
+        this.age = 31;
+        console.log(this.type)
+    }
+}
+const myPrint = new EvgenyClass("Anna", "Super");
+console.log(myPrint);
+
+// 61. Inheritance Wrap Up
