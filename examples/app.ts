546416a46fb16a2b041e7d45fc38c45950d82e09
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-08-26 10:20:13 +0200

    upd

--- examples/app.ts
+++ examples/app.ts
@@ -37,6 +37,18 @@ hisPerson.firstName = "Evgeny";
 
 const newPerson = new Person2();
 newPerson.firstName = "Zhenja";
-greet(newPerson);
+hello(newPerson);
+newPerson.greet("Vasechkin");
 
+// Function Types
 
+interface DoubleValueFunc {
+    (number1: number, number2: number): number;
+}
+
+let myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function (value1: number, value2: number) {
+    return (value1 + value2) * 2;
+};
+
+console.log(myDoubleFunction(10, 20));
