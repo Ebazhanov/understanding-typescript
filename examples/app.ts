08203573352ec307f87358b74e46c35f38a6932d
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-08-27 09:29:44 +0200

    upd

--- examples/app.ts
+++ examples/app.ts
@@ -52,3 +52,18 @@ myDoubleFunction = function (value1: number, value2: number) {
 };
 
 console.log(myDoubleFunction(10, 20));
+
+// Interface Inheritance
+interface AgedPerson extends NamedPerson {
+    age: number;
+}
+
+const oldPerson: AgedPerson = {
+    age: 27,
+    firstName: "Kolja",
+    greet(lastName: string){
+        console.log("Hello!")
+    }
+};
+
+console.log(oldPerson);
