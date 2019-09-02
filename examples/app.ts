30b58741281ba0b604180e76374e74a0a91442a6
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-09-02 09:26:31 +0200

    upd

--- examples/app.ts
+++ examples/app.ts
@@ -22,9 +22,10 @@ testResultsNew.push(-2.999);
 console.log(testResultsNew);
 
 // Arrays
-function printAll<T>(args: T[]){
+function printAll<T>(args: T[]) {
     args.forEach((element) => console.log(element))
 }
+
 printAll<string>(["Apple", "Banana"]);
 
 // Generic Types
@@ -32,15 +33,15 @@ const echo2: <T>(data: T) => T = betterEcho;
 console.log(echo2<string>("Something"));
 
 // Generic Class
-class SimpleMath<T> {
+class SimpleMath<T extends number | string, U extends number | string> {
     baseValue: T | undefined;
-    multiplyValue: T | undefined;
-/*    calculate(): number{
-        return + this.baseValue * + this.multiplyValue;
-    }*/
+    multiplyValue: U | undefined;
+    calculate(): number {
+        return +this.baseValue! * +this.multiplyValue!;
+    }
 }
 
-const simpleMath = new SimpleMath();
+const simpleMath = new SimpleMath<string, number>();
 simpleMath.baseValue = 'text';
 simpleMath.multiplyValue = 21;
-// console.log(simpleMath.calculate());
+console.log(simpleMath.calculate());
