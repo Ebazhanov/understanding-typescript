b257b9e185e067752fa25f3e63d5df8c7e6db895
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-09-05 09:57:17 +0200

    upd

--- examples/app_decorators.ts
+++ examples/app_decorators.ts
@@ -8,3 +8,31 @@ class PersonDecoration {
         console.log("WuHuwhooo");
     }
 }
+
+// Factory
+function logging (value: boolean){
+    return value ? logged: null;
+}
+
+// @ts-ignore
+@logging(true)
+class CarWithDecoration {
+
+}
+
+// Advanced
+function printable(constructorFn: Function) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    }
+}
+
+
+// @ts-ignore
+// @logging(true)
+@printable
+class PlantWithDecorator {
+    name = "Green Plant";
+}
+const plantNew = new PlantWithDecorator();
+(<any>plantNew).print();
